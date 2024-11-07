dbc73ea1c8f86b5b81f9f42d5a52a4159faf3986
Zoltán Szűcs <zoltan76szucs@gmail.com>
2024-11-07 14:36:39 +0100

    Data Fetching P11

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const UsersPage = () => {
-  return (
-    <div>UsersPage</div>
-  )
+interface User {
+  id: number;
+  name: string;
 }
 
-export default UsersPage
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
+  return (
+    <>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default UsersPage;
