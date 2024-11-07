3d5b8e51672ad5ea6088989b7f1370603993075d
Zoltán Szűcs <zoltan76szucs@gmail.com>
2024-11-07 14:02:29 +0100

    Routing and Navigation  P9

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main><h1>Hello world!</h1></main>
-      );
+    <main>
+      <h1>Hello world!</h1>
+      <Link href="/users">Users</Link>
+    </main>
+  );
 }
