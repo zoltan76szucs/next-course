cccd7766bb4e906ee69c66e1d93935aced9fd41e
Zoltán Szűcs <zoltan76szucs@gmail.com>
2024-11-07 14:15:46 +0100

    Client and Server Components  P10

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button onClick={() => console.log("Button click")}> Add to card</button>
+    </div>
+  );
+};
+
+export default AddToCart;
