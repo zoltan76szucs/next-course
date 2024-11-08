c43ac27636bff70e9a8546b025b7daa701991449
Zoltán Szűcs <zoltan76szucs@gmail.com>
2024-11-08 16:01:43 +0100

    Layouts P25

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import NavBar from "./NavBar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <NavBar></NavBar>
+        <main className="p-5"> {children} </main>
       </body>
     </html>
   );
