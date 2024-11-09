ae54a5b2a673dd2e67fc146a5eeefc64cc1d392b
Zoltán Szűcs <zoltan76szucs@gmail.com>
2024-11-09 11:21:33 +0100

    Deleting an Object  P35

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -14,20 +14,34 @@ export function GET(
 }
 
 export async function PUT(
-    request: NextRequest,
-    { params }: { params: Promise<{ id: string }> } // params Promise-ként van megadva
-  ) {
-    const body = await request.json();
-    const { id: idStr } = await params; // await használata itt a params kibontásához
-    const id = parseInt(idStr, 10); // konvertáljuk számmá
-  
-    if (!body.name) {
-      return NextResponse.json({ error: "Name is required" }, { status: 400 });
-    }
-  
-    if (id > 10) {
-      return NextResponse.json({ error: "User not found" }, { status: 404 });
-    }
-  
-    return NextResponse.json({ id, name: body.name });
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> } // params Promise-ként van megadva
+) {
+  const body = await request.json();
+  const { id: idStr } = await params; // await használata itt a params kibontásához
+  const id = parseInt(idStr, 10); // konvertáljuk számmá
+
+  if (!body.name) {
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
   }
+
+  if (id > 10) {
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
+
+  return NextResponse.json({ id, name: body.name });
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> } // params Promise-ként van megadva
+) {
+  const { id: idStr } = await params; // await használata itt a params kibontásához
+  const id = parseInt(idStr, 10); // konvertáljuk számmá
+
+  if (id > 10) {
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
+
+  return NextResponse.json({ id });
+}
