335bd20a6c1ac52b93aa1e5f40b5477eaaff4cca
Zoltán Szűcs <zoltan76szucs@gmail.com>
2024-11-09 21:36:48 +0100

    Getting Data  P44

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,17 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "../schema";
+import prisma from "@/prisma/client";
 
-export function GET(
+export async function GET(
   request: NextRequest,
-  { params }: { params: { id: number } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  if (params.id > 10)
+  const { id: idStr } = await params; // await használata itt a params kibontásához
+  const id = parseInt(idStr, 10); // konvertáljuk számmá
+
+  const user = await prisma.user.findUnique({ where: { id: id } });
+
+  if (!user)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
 
-  return NextResponse.json([
-    { id: 1, name: "Zotya" },
-    { id: 2, name: "Gabi" },
-  ]);
+  return NextResponse.json(user);
 }
 
 export async function PUT(
--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,11 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "@/prisma/client";
 
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: "Zotya" },
-    { id: 2, name: "Gabi" },
-  ]);
+
+export async function GET(request: NextRequest) {
+  const users = await prisma.user.findMany();
+
+  return NextResponse.json(users);
 }
 
 export async function POST(request: NextRequest) {
