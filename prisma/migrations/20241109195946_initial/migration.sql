ebce543bd57d223eb73197e12fa6693f7a0ad3f2
Zoltán Szűcs <zoltan76szucs@gmail.com>
2024-11-09 21:08:51 +0100

    Connect to SQL DB and Creating Migrations P42

---
+++ prisma/migrations/20241109195946_initial/migration.sql
@@ -0,0 +1,27 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- CreateTable
+CREATE TABLE [dbo].[User] (
+    [id] INT NOT NULL IDENTITY(1,1),
+    [email] NVARCHAR(1000) NOT NULL,
+    [name] NVARCHAR(1000) NOT NULL,
+    [followers] INT NOT NULL CONSTRAINT [User_followers_df] DEFAULT 0,
+    [isActive] BIT NOT NULL CONSTRAINT [User_isActive_df] DEFAULT 1,
+    CONSTRAINT [User_pkey] PRIMARY KEY CLUSTERED ([id]),
+    CONSTRAINT [User_email_key] UNIQUE NONCLUSTERED ([email])
+);
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
