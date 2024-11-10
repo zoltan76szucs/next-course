e1f0bc8e3cb64424e72114a5dde9e14f2b38a14e
Zoltán Szűcs <zoltan76szucs@gmail.com>
2024-11-10 18:32:20 +0100

    Creating Data   P45

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -2,7 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
 import prisma from "@/prisma/client";
 
-
 export async function GET(request: NextRequest) {
   const users = await prisma.user.findMany();
 
@@ -15,5 +14,19 @@ export async function POST(request: NextRequest) {
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
 
-  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+  const user = await prisma.user.findUnique({ where: { email: body.email } });
+
+  if (user)
+    return NextResponse.json(
+      { error: "User already exists." },
+      { status: 400 }
+    );
+
+  const newuser = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+  return NextResponse.json(newuser, { status: 201 });
 }
