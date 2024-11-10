72bfb5469fea5a442dbbf8bcbbe9121ceaafbd1f
Zoltán Szűcs <zoltan76szucs@gmail.com>
2024-11-10 18:40:09 +0100

    Updating Data   P46

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -21,8 +21,10 @@ export async function PUT(
   request: NextRequest,
   { params }: { params: Promise<{ id: string }> } // params Promise-ként van megadva
 ) {
-  const body = await request.json();
-  const { id: idStr } = await params; // await használata itt a params kibontásához
+
+  
+  const body = await request.json();  
+  const { id: idStr } = await params; // await használata itt a params kibontásához  
   const id = parseInt(idStr, 10); // konvertáljuk számmá
 
   const validation = schema.safeParse(body);
@@ -31,11 +33,20 @@ export async function PUT(
     return NextResponse.json(validation.error.errors, { status: 400 });
   }
 
-  if (id > 10) {
-    return NextResponse.json({ error: "User not found" }, { status: 404 });
-  }
+  const user = await prisma.user.findUnique({ where: { id: id } });
 
-  return NextResponse.json({ id, name: body.name });
+  if (!user)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  const updatedUser = await prisma.user.update({
+    where: { id: user.id },
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+
+  return NextResponse.json(updatedUser);
 }
 
 export async function DELETE(
