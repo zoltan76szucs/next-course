a8dcb5a1a1b7ceaffe371a59d7565896418088d6
Zoltán Szűcs <zoltan76szucs@gmail.com>
2024-11-11 10:54:26 +0100

    Showing Uploaded ImagesCustomizing the Upload Widget (Cloudinary)  P53

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -22,6 +22,34 @@ const UploadPage = () => {
       )}
       <CldUploadWidget
         uploadPreset="hq0y5lho"
+        options={{
+          sources: ["local"],
+          multiple: false,
+          styles: {
+            palette: {
+              window: "#F5F5F5",
+              sourceBg: "#FFFFFF",
+              windowBorder: "#90a0b3",
+              tabIcon: "#0094c7",
+              inactiveTabIcon: "#69778A",
+              menuIcons: "#0094C7",
+              link: "#53ad9d",
+              action: "#8F5DA5",
+              inProgress: "#0194c7",
+              complete: "#53ad9d",
+              error: "#c43737",
+              textDark: "#000000",
+              textLight: "#FFFFFF",
+            },
+            fonts: {
+              default: null,
+              "'Poppins', sans-serif": {
+                url: "https://fonts.googleapis.com/css?family=Poppins",
+                active: true,
+              },
+            },
+          },
+        }}
         onSuccess={(result, { widget }) => {
           if (result.event !== "success") return;
 
