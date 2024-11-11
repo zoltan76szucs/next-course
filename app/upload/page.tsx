87fef6c4e3ec6f3a33d7f155a177897c13fce0e8
Zoltán Szűcs <zoltan76szucs@gmail.com>
2024-11-11 10:47:00 +0100

    Showing Uploaded Images  P53

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,18 +1,45 @@
 "use client";
-import React from "react";
-import { CldUploadWidget } from "next-cloudinary";
+import React, { useState } from "react";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+
+interface CloudinaryResult {
+  public_id: string;
+}
 
 const UploadPage = () => {
+  console.log("--------------------------");
+
+  const [publicId, setPublicId] = useState("");
   return (
-    <CldUploadWidget uploadPreset="hq0y5lho">
-      {({ open }) => {
-        return (
-          <button className="btn btn-primary" onClick={() => open()}>
-            Upload an Image
-          </button>
-        );
-      }}
-    </CldUploadWidget>
+    <>
+      {publicId && (
+        <CldImage
+          src={publicId}
+          width={270}
+          height={180}
+          alt="my image"
+        ></CldImage>
+      )}
+      <CldUploadWidget
+        uploadPreset="hq0y5lho"
+        onSuccess={(result, { widget }) => {
+          if (result.event !== "success") return;
+
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+
+          console.log("onSuccess1", result); // { public_id, secure_url, etc }
+        }}
+      >
+        {({ open }) => {
+          return (
+            <button className="btn btn-primary" onClick={() => open()}>
+              Upload an Image
+            </button>
+          );
+        }}
+      </CldUploadWidget>
+    </>
   );
 };
 
