03220eb642324b312bb54d01a74038c203ac5f71
Zoltán Szűcs <zoltan76szucs@gmail.com>
2024-11-11 13:12:26 +0100

    Accessing Sessions on the Client  P59

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,13 @@
+"use client";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
+  const { status, data: sesssion } = useSession();
+
+  if (status === "loading") return null;
+
   return (
     <div className="flex bg-slate-200 p-5 space-x-3">
       {" "}
@@ -9,7 +15,11 @@ const NavBar = () => {
         Next.js
       </Link>
       <Link href="/users">Users</Link>
-      <Link href="/api/auth/signin">Login</Link>
+      {status === "loading" && <div> Loading</div>}
+      {status === "authenticated" && <div> {sesssion.user!.name}</div>}
+      {status === "unauthenticated" && (
+        <Link href="/api/auth/signin">Login</Link>
+      )}
     </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import NavBar from "./NavBar";
+import AuthProvider from "./auth/Provider";
 
 
 const geistSans = localFont({
@@ -30,12 +31,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <NavBar></NavBar>
-        <main className="p-5">
-          {/* <Suspense fallback={<p>Loading....</p>}> */}
-          {children}
-          {/* </Suspense> */}
-        </main>
+        <AuthProvider>
+          <NavBar></NavBar>
+          <main className="p-5">
+            {/* <Suspense fallback={<p>Loading....</p>}> */}
+            {children}
+            {/* </Suspense> */}
+          </main>
+        </AuthProvider>
       </body>
     </html>
   );
