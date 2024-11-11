99ae56646ae3cb2c75869efe8915b3ac3d02974f
Zoltán Szűcs <zoltan76szucs@gmail.com>
2024-11-11 12:25:18 +0100

    Configuring Google Provider   P57

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,12 +3,13 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <div className="flex bg-slate-200 p-5">
+    <div className="flex bg-slate-200 p-5 space-x-3">
       {" "}
       <Link href="/" className="mr-5">
         Next.js
       </Link>
       <Link href="/users">Users</Link>
+      <Link href="/api/auth/signin">Login</Link>
     </div>
   );
 };
