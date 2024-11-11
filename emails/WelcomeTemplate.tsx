156433df8c5bb3bc2dd9d23dc44286c3775859cc
Zoltán Szűcs <zoltan76szucs@gmail.com>
2024-11-11 17:26:01 +0100

    Styling Emails P70

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -1,26 +1,37 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 
 import {
   Html,
   Body,
+  Tailwind,
   Container,
   Text,
   Link,
   Preview,
 } from "@react-email/components";
 
-const WelcomeTemplate = ({name}: {name : string}) => {
+const WelcomeTemplate = ({ name }: { name: string }) => {
   return (
     <Html>
       <Preview>Welcome on borat</Preview>
-      <Body>
-        <Container>
-          <Text>Hello {name}</Text>
-          <Link href="https://index.hu">index page</Link>
-        </Container>
-      </Body>
+      <Tailwind>
+        <Body className="bg-white">
+          <Container>
+            <Text className="font-bold text-3xl">Hello {name}</Text>
+            <Link href="https://index.hu">index page</Link>
+          </Container>
+        </Body>
+      </Tailwind>
     </Html>
   );
+};
+
+const body: CSSProperties = {
+  background: "#fff",
+};
+
+const heading: CSSProperties = {
+  fontSize: "32px",
 };
 
 export default WelcomeTemplate;
