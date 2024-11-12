9f03fe107dcbc9094d0233d109abfc7418924eed
Zoltán Szűcs <zoltan76szucs@gmail.com>
2024-11-12 09:57:56 +0100

    Optimizing images  P73

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,25 @@
-import Link from "next/link";
-import ProductCard from "./components/ProductCard";
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+import Image from "next/image";
+import next from "@/public/images/next.svg";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions)
   return (
-    <main>
-      <h1>Hello {session && <span>{session.user?.name}</span>}!</h1>
-      <Link href="/users">Users</Link>
-      <ProductCard></ProductCard>
+    <main className="relative h-screen">
+      <Image
+        src="https://bit.ly/react-cover"
+        alt="next"
+        fill
+        // style={{ objectFit: "cover" }}
+        // style={{ objectFit: "contain" }}
+        className="object-cover"
+        // sizes="100vw"
+        //set mobil, table, .. sizes
+        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
+
+        quality={100}
+        priority>
+
+        </Image>
+
     </main>
   );
 }
